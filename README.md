ed4f28b544526c3a4bb194c9e59f0ec0d58a4923
masudrana <mdmasudrana1999bd@gmail.com>
2024-03-18 02:38:03 +0600

    Simple changes

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+First changed
---
+++ empty.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    cout << "Hello World\n";
+}
+
+signed main() {
+   ios_base::sync_with_stdio (0);
+   cin.tie (0);
+
+   int t = 1;  // cin >> t;
+   for (int tc = 1; tc <= t; tc++) {
+      //cout<<"Case "<<tc<<": ";
+      solve();
+   }
+   return 0;
+}
